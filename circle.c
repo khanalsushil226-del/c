7158da71642ac8ffc2789424152e99ba19e212a8
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-17 22:23:18 +0545

    variables things

---
+++ circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int radius;
+    float pie = 3.14;
+    printf("enter radius of circle");
+    scanf("%d", &radius);
+    printf("area of circle is: %f", pie*radius*radius);
+    return 0;
+
+}
