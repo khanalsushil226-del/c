2a51f64a7a06eb5b0ab59ffc5248a75d373328c1
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-17 22:31:59 +0545

    asking and calculating cube

---
+++ cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the value of n:\n");
+    scanf("%d",&n);
+     int cube = n*n*n;
+    printf("cube of entered number is: %d", cube);
+    return 0;
+}
