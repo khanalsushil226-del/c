9de8c81d3f7f74cc9de030ad1217c8d71b8f170b
khanalsushil226-del <khanalsushil226@gmail.com>
2026-08-17 22:28:55 +0545

    understanding and playing with variables

---
+++ perimeter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter the value of a");
+    scanf("%d", &a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    printf("perimeter of rectangle is: %d", 2*(a+b));
+    return 0;
+}
